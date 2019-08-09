5db51fe330c2cd9c0e072e97f5531491b9f432bf
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-09 09:20:32 -0700

    back to functional; copy and edit boilerplate

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { FormikForm, Field, withFormik } from 'formik';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
-class Form extends React.Component {
-  constructor() {
-    super();
+const raw = (touched, errors) => {
+  return (
+    <Form>
+      <div>
+        <Field type='text' name='name' placeholder='name' />
+        {touched.name && errors.name && <p>{errors.name}</p>}
+      </div>
+      <div>
+        <Field type='password' name='password' placeholder='password' />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+      </div>
+      <button type='submit'>submit</button>
+    </Form>
+  );
+};
+
+export default withFormik({
+  mapPropsToValues(props) {
+    return {
+      name: props.name || '',
+      password: props.password || '',
+      subFun: props.subFun
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    name: Yup
+      .string()
+      .min(1, "name can't be empty")
+      .required(),
+    password: Yup
+      .string()
+      .min(5, "password needs at least 5 characters")
+      .required()
+  }),
+
+  handleSubmit(vals, { resetForm, setSubmitting }) {
+    // axios
+    //   .post('', vals)
+    //   .then(res => {
+    //     vals.subFun(res.data);
+    //     resetForm();
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //     setSubmitting(false);
+    //   });
+    alert('form submitted');
   }
-
-  render() {
-    return (
-      <p>hello</p>
-    );
-  }
-}
-
-export default Form;
+})(raw);
